48a611c22412cb6adc2bcdb8b8fc8f149d442ade
Lacyg <lacyg@cs2.ch>
2016-02-03 09:35:56 +0100

    Update ext_tables.sql
    BE speedup

--- ext_tables.sql
+++ ext_tables.sql
@@ -10,3 +10,28 @@ CREATE TABLE tt_content (
   KEY index_fluxparent (tx_flux_parent),
   KEY index_fluxparentcolumn (tx_flux_column(12),tx_flux_parent)
 );
+
+#
+# Table structure for table 'cf_flux_preview_cache'
+#
+CREATE TABLE cf_flux_preview_cache (
+    id INT(11) unsigned NOT NULL AUTO_INCREMENT,
+    identifier VARCHAR(250) DEFAULT '' NOT NULL,
+    crdate INT(11) unsigned DEFAULT '0' NOT NULL,
+    content mediumblob,
+    expires INT(11) unsigned DEFAULT '0' NOT NULL,
+    PRIMARY KEY (id),
+    KEY cache_id (identifier)
+) ENGINE=InnoDB;
+
+#
+# Table structure for table 'cf_flux_preview_cache_tags'
+#
+CREATE TABLE cf_flux_preview_cache_tags (
+    id INT(11) unsigned NOT NULL AUTO_INCREMENT,
+    identifier VARCHAR(250) DEFAULT '' NOT NULL,
+    tag VARCHAR(250) DEFAULT '' NOT NULL,
+    PRIMARY KEY (id),
+    KEY cache_id (identifier),
+    KEY cache_tag (tag)
+) ENGINE=InnoDB;
